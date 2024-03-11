bc35ce49b29563b5f10bf0a0fb5f5ead27e44d4b
Chaion Saha <chaionsaha00@gmail.com>
2024-03-11 17:04:44 +0600

    pages and contact links are organized to different files

---
+++ components/global/links.js
@@ -0,0 +1,41 @@
+export const pages = [
+    {
+        "name": 'Portfolio',
+        "link": '/portfolio',
+        "icon": "bi bi-distribute-vertical"
+    },
+    {
+        "name": 'Services',
+        "link": '/services',
+        "icon": "bi bi-grid-fill"
+    },
+    {
+        "name": 'Team',
+        "link": '/team',
+        "icon": "bi bi-people-fill"
+    },
+    {
+        "name": 'Contact',
+        "link": '/contact',
+        "icon": "bi bi-envelope-fill"
+    },
+]
+
+export const contactLinks = [
+    {
+        'link': 'https://www.linkedin.com/company/pixel-dynamics-studio/about/',
+        'icon': 'bi bi-linkedin'
+    },
+    {
+        'link': 'https://www.facebook.com/profile.php?id=61550213669968',
+        'icon': 'bi bi-facebook'
+    },
+    {
+        'link': 'https://www.instagram.com/pixeldynamics.studio/',
+        'icon': 'bi bi-instagram'
+    },
+    {
+        'link': 'https://twitter.com/Pixel_Dynamics',
+        'icon': 'bi bi-twitter-x'
+    }
+]
--- components/global/sidebar.js
+++ components/global/sidebar.js
@@ -3,48 +3,8 @@ import logo from '@/assets/logo-gif.gif'
 import Image from 'next/image';
 import Link from "next/link";
 import {useRouter} from "next/router";
+import {contactLinks, pages} from "@/components/global/links";
 
-const pages = [
-    {
-        "name": 'Portfolio',
-        "link": '/portfolio',
-        "icon": "bi bi-distribute-vertical"
-    },
-    {
-        "name": 'Services',
-        "link": '/services',
-        "icon": "bi bi-grid-fill"
-    },
-    {
-        "name": 'Team',
-        "link": '/team',
-        "icon": "bi bi-people-fill"
-    },
-    {
-        "name": 'Contact',
-        "link": '/contact',
-        "icon": "bi bi-envelope-fill"
-    },
-]
-
-const contactLinks = [
-    {
-        'link': 'https://www.linkedin.com/company/pixel-dynamics-studio/about/',
-        'icon': 'bi bi-linkedin'
-    },
-    {
-        'link': 'https://www.facebook.com/profile.php?id=61550213669968',
-        'icon': 'bi bi-facebook'
-    },
-    {
-        'link': 'https://www.instagram.com/pixeldynamics.studio/',
-        'icon': 'bi bi-instagram'
-    },
-    {
-        'link': 'https://twitter.com/Pixel_Dynamics',
-        'icon': 'bi bi-twitter-x'
-    }
-]
 const Sidebar = () => {
     const router = useRouter();
 
