541581e43f4cf16145983262a0df5e56f613a6c6
Chaion Saha <chaionsaha00@gmail.com>
2024-04-03 16:53:40 +0600

    updated some files

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,28 +1,27 @@
 import Title from '@/components/Shared/title';
-import {getDatabase} from "@/db/mongoConnection";
-import {useEffect} from "react";
+import { getDatabase } from '@/db/mongoConnection';
 
-const Services = ({services}) => {
-    useEffect(() => {
-        console.log(services);
-    }, [services]);
-    return (
-        <div className='container'>
-            <Title title='Services'/>
-            Services Page
-        </div>
-    );
+const Services = ({ services = [] }) => {
+	return (
+		<div className='container'>
+			<Title title='Services' />
+			Services Page
+		</div>
+	);
 };
 
 export default Services;
 
-export async function getServerSideProps(context) {
-    const db = await getDatabase();
-    const servicesCollection = db.collection('services');
-    const serviceList = await servicesCollection.find().project({_id: 0}).toArray();
-    return {
-        props: {
-            services: serviceList
-        }
-    }
+export async function getServerSideProps() {
+	const db = await getDatabase();
+	const servicesCollection = db.collection('services');
+	const serviceList = await servicesCollection
+		.find()
+		.project({ _id: 0 })
+		.toArray();
+	return {
+		props: {
+			services: serviceList,
+		},
+	};
 }
