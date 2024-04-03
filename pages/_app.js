71a3a6d564676190da03133cf7f197b2f94f71be
Chaion Saha <chaionsaha00@gmail.com>
2024-04-04 00:45:46 +0600

    added progress bar for route change

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
 import "@/styles/globals.css";
 import {NextUIProvider} from "@nextui-org/react";
 import Layout from "@/components/global/Layout";
-import NextProgress from "next-progress";
 import {AnimatePresence} from "framer-motion";
 import {useRouter} from "next/router";
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({Component, pageProps}) {
     const router = useRouter();
     return (
         <NextUIProvider>
-            <NextProgress delay={300} options={{showSpinner: false}}/>
+            <NextNProgress/>
             <Layout>
                 <AnimatePresence onExitComplete={() => window.scrollTo(0, 0)} mode="wait" initial={false}>
                     <Component {...pageProps} key={router.asPath}/>
