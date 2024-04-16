0b3eb7575881a21724e0713c78001460c892965e
Chaion Saha <chaionsaha00@gmail.com>
2024-04-17 03:41:35 +0600

    scroll effect issue fixed

--- components/global/Layout.js
+++ components/global/Layout.js
@@ -3,17 +3,26 @@ import Head from "next/head";
 import {useEffect, useRef, useState} from "react";
 import {useRouter} from "next/router";
 import axios from "axios";
-import {signOut} from "next-auth/react";
 
 const Layout = ({children}) => {
     const [active, setActive] = useState(false);
     const router = useRouter();
     const topDivRef = useRef(null);
 
+    useEffect(() => {
+        const handleRouteChange = () => {
+            if (topDivRef.current) {
+                topDivRef.current.scrollIntoView({behavior: "smooth"});
+            }
+        };
+        router.events.on('routeChangeComplete', handleRouteChange);
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router]);
 
     useEffect(() => {
         setActive(false);
-
         if (router.pathname.includes('/admin') && !router.pathname.includes('/admin/auth')) {
             axios('/api/get-account-status').then(res => {
 
@@ -21,28 +30,17 @@ const Layout = ({children}) => {
                 if (response.status === 403) {
                     router.push('/admin/auth/create-account-success');
                 } else if (response.status === 401) {
-                    signOut();
                     router.push('/admin/auth/login');
                 }
             });
         }
 
-        const handleRouteChange = () => {
-            if (topDivRef.current) {
-                topDivRef.current.scrollIntoView();
-            }
-        };
-        router.events.on('routeChangeComplete', handleRouteChange);
-        return () => {
-            router.events.off('routeChangeComplete', handleRouteChange);
-        };
-
 
     }, [router]);
 
+
     return (
         <div className='flex h-[100vh] relative overflow-x-hidden lg:overflow-x-auto'>
-            <div ref={topDivRef}></div>
             <Head>
                 <link rel="icon" type="image/x-icon"
                       href="https://i.ibb.co/4Wh3gKQ/logo.png"/>
@@ -55,7 +53,10 @@ const Layout = ({children}) => {
                             <Sidebar active={active} setActive={setActive}/>
                         </div>
                         <div
-                            className={`lg:w-[84%] w-full h-full duration-500 delay-150 overflow-y-auto  ${active ? 'translate-x-[70%] md:translate-x-[40%] lg:translate-x-0' : 'translate-x-0 lg:translate-x-0'} ${router.pathname.includes('/admin') ? 'bg-admin-primary text-base-200' : ''}`}>{children}</div>
+                            className={`lg:w-[84%] w-full h-full duration-500 delay-150 overflow-y-auto  ${active ? 'translate-x-[70%] md:translate-x-[40%] lg:translate-x-0' : 'translate-x-0 lg:translate-x-0'} ${router.pathname.includes('/admin') ? 'bg-admin-primary text-base-200' : ''}`}>
+                            <div ref={topDivRef}></div>
+                            {children}
+                        </div>
                     </> : <div className={` w-full h-full bg-[#161B21] text-base-200`}>{children}</div>
 
             }
